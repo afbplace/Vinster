a257c87197fb389254da1527b9ceb74675948923
Alex Place <afbplace@gmail.com>
2014-03-30 15:31:45 +0000

    Support Creation

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,31 @@
 class PostsController < ApplicationController
 
-		def index
-			#everything here will get run 
-			#when someone goes to /posts
+	def index
+		
 
-			@posts = Post.all 
-		end
+		@posts = Post.all 
+	end
+
+	def new
+		@post = Post.new
+	end
+
+	def create 
+			Post.create( post_params ) 
+
+			redirect_to posts_path
+
+			
+	end
 
 end
+
+private
+
+	def post_params
+		params.require(:post).permit(:title, :description)
+
+	end
+
+
+
